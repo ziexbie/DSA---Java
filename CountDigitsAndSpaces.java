f8cc409bf47c9be75e30ea10cfbfec955ff4d2df
ziexbie <ziexbie@gmail.com>
2025-11-20 19:21:23 +0530

    Count digits and spaces

--- CountDigitsAndSpaces.java
+++ CountDigitsAndSpaces.java
@@ -4,7 +4,15 @@ public class CountDigitsAndSpaces {
         int d = 0;
         int space = 0;
 
-        
-
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch>='0' && ch<='9'){
+                d++;
+            } else if (ch == ' '){
+                space++;
+            }
+        }
+       System.out.println(d);
+       System.out.println(space);
     }
 }
